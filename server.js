93c3e618486fb0490e861e35efdc4c4d3f96cfe9
Peter Obijiofor <108196402+DoingsLLC@users.noreply.github.com>
2024-04-05 15:28:40 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+import { createServer } from 'http';
+import { readFile } from 'fs';
+
+const server = createServer((req, res) => {
+  readFile('./index.html', (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Error loading index.html');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
